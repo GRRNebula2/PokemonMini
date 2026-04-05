0dd9a6862a03d4efa82f2d29329ee7f8938b1c71
GRRNebula2 <pahkina.katainen@protonmail.com>
2026-04-05 14:19:15 +0300

    Npc ja pelaaja vaihdettu päittäin. Pelaaja on nyt tyttö.

--- assetLoader.js
+++ assetLoader.js
@@ -1,9 +1,9 @@
 function loadAssets() {
   loadSpriteAtlas("./assets/characters.png", {
-    "player-down": { x: 0, y: 82, width: 16, height: 16 },
-    "player-up": { x: 16, y: 82, width: 16, height: 16 },
+    "player-down": { x: 32, y: 82, width: 16, height: 16 },
+    "player-up": { x: 48, y: 82, width: 16, height: 16 },
     "player-side": {
-      x: 0,
+      x: 32,
       y: 98,
       width: 32,
       height: 16,
@@ -11,7 +11,7 @@ function loadAssets() {
       sliceY: 1,
       anims: { walk: { from: 0, to: 1, speed: 6 } },
     },
-    "npc": { x: 32, y: 98, width: 16, height: 16 },
+    "npc": { x: 0, y: 98, width: 16, height: 16 },
     "cat-mon": { x: 0, y: 16, width: 32, height: 32 },
     "spider-mon": { x: 32, y: 16, width: 32, height: 32 },
     "centipede-mon": { x: 64, y: 16, width: 32, height: 32 },
